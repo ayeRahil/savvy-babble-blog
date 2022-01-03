733243cf982b7308bdfefcfa2439942258e3445b
Rahil Inamdar <rahilinamdar007@gmail.com>
2022-01-03 21:29:26 +0530

    'a'

--- main/urls.py
+++ main/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('articles/', views.HomeView.as_view(template_name='main/allarticles.html'), name='all-article'),
     path('authors/', views.AuthorView.as_view(), name='authors'),
     path('article/<int:pk>', views.PostDetailView.as_view(), name='article-detail'),
-    path('add_article/', views.CreateArticleView.as_view(), name='add-article'),
+    path('add_article/', views.CreateArticleView, name='add-article'),
     path('add_category/', views.CreateCategoryView.as_view(), name='add-category'),
     path('category/<str:cats>', views.CategoryView, name='category'),
     path('article/update/<int:pk>', views.UpdateArticleView.as_view(), name='update-article'),
